c46b4d7b1dd04a402d3c40bf57caad80d7035bca
trilvid <trillztech34@gmail.com>
2024-08-21 12:16:13 +1200

    added a new tidio account link

--- resources/views/layouts/front_master.blade.php
+++ resources/views/layouts/front_master.blade.php
@@ -386,7 +386,7 @@
     }
     </script>
 <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
-<script src="//code.tidio.co/32wppxte4ibd3o5hvw09tlaqfvgbovcq.js" async></script>
+<script src="//code.tidio.co/uqepgovjdhbgv2ehzonuuh97ratzwbxx.js" async></script>
 
 @yield('scripts')
 
